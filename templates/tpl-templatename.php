0d625771eb8cb25ee65877798bb4d7f70cbd37b4
Carl Alberto <cralberto11@gmail.com>
2018-02-05 19:42:26 +0800

    updated the custom template

--- templates/tpl-templatename.php
+++ templates/tpl-templatename.php
@@ -7,25 +7,46 @@
 
 get_header(); ?>
 	<div class="row">
-		<?php
-		if ( ! bootstrapfast_main_sidebar_placement() ) {
-			get_sidebar();
-		}
-		?>
 		<div id="primary" class="content-area col">
 			<main id="main" class="site-main" role="main">
 
 				<?php
 				while ( have_posts() ) :
 					the_post();
+					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header class="entry-header">
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						</header><!-- .entry-header -->
 
-					get_template_part( 'template-parts/content', 'page' );
+						<div class="entry-content">
+							<?php
+								the_content();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+								wp_link_pages( array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrapfast' ),
+									'after'  => '</div>',
+								) );
+							?>
+						</div><!-- .entry-content -->
 
+						<?php if ( get_edit_post_link() ) : ?>
+							<footer class="entry-footer">
+								<?php
+									edit_post_link(
+										sprintf(
+											/* translators: %s: Name of current post */
+											esc_html__( 'Edit %s', 'bootstrapfast' ),
+											the_title( '<span class="screen-reader-text">"', '"</span>', false )
+										),
+										'<span class="edit-link">',
+										'</span>'
+									);
+								?>
+							</footer><!-- .entry-footer -->
+						<?php endif; ?>
+					</article><!-- #post-## -->
+					<?php
 				endwhile; // End of the loop.
 				?>
 
